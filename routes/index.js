aeae60b5521505c15ddf31a4bd85afda49ada4f8
Pablo Sanchez <pablosy@gmail.com>
2017-05-24 11:38:01 +0200

    Arreglado problema router

--- routes/index.js
+++ routes/index.js
@@ -32,49 +32,7 @@ router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
 //Definicion de rutas de practica52
 router.get('/quizzes/randomplay', quizController.practica52);
-router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
-
-
-// Pagina de ayuda
-router.get('/help', function(req, res, next) {
-    res.render('help');
-});
-
-var express = require('express');
-var router = express.Router();
-
-var quizController = require('../controllers/quiz_controller');
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
-
-// Pagina de creditos
-router.get('/author', function(req, res, next) {
-    res.render('author');
-});
-
-
-// Autoload de rutas que usen :quizId
-router.param('quizId', quizController.load);
-
-
-// Definición de rutas de /quizzes
-router.get('/quizzes',                     quizController.index);
-router.get('/quizzes/:quizId(\\d+)',       quizController.show);
-router.get('/quizzes/new',                 quizController.new);
-router.post('/quizzes',                    quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
-router.put('/quizzes/:quizId(\\d+)',       quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
-
-router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
-router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
-
-//Definicion de rutas de practica52
-router.get('/quizzes/randomplay', quizController.practica52);
-router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.check);
+router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.comprueba);
 
 
 // Pagina de ayuda
